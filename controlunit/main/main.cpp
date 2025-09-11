d459d1dc9724c56b9af8c95da0ceba031d65e191
Erik Dahl <erik@iunderlandet.se>
2025-09-11 16:57:41 +0200

    refactor: include missed main folder refactoring rest_server

--- controlunit/main/main.cpp
+++ controlunit/main/main.cpp
@@ -1,5 +1,7 @@
 #include "wifi_config.h"
 #include "wifi_manager.h"
+#include "rest_server.h"
+#include "hello_handler.h"
 #include <esp_http_server.h>
 #include <esp_log.h>
 #include <esp_wifi.h>
@@ -10,34 +12,9 @@
 
 
 
-static const char *TAG = "web_server";
-
-esp_err_t hello_get_handler(httpd_req_t *req) {
-    const char* resp_str = "Hello from separate files ESP32!";
-    httpd_resp_send(req, resp_str, strlen(resp_str));
-    return ESP_OK;
-}
-
 extern "C" void app_main(void) {
-    ESP_LOGI(TAG, "Starting Wi-Fi");
-    nvs_flash_init();  // Required for Wi-Fi and NVS
+    nvs_flash_init();
     init_wifi();
+    start_rest_server();
 
-    ESP_LOGI(TAG, "Starting HTTP server");
-
-    httpd_handle_t server = NULL;
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
-    if (httpd_start(&server, &config) == ESP_OK) {
-        httpd_uri_t hello_uri = {
-            .uri      = "/hello",
-            .method   = HTTP_GET,
-            .handler  = hello_get_handler,
-            .user_ctx = NULL
-        };
-        httpd_register_uri_handler(server, &hello_uri);
-        ESP_LOGI(TAG, "Server started");
-    } else {
-        ESP_LOGE(TAG, "Failed to start server");
-    }
 }
