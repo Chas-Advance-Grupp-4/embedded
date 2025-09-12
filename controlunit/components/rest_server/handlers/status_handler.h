76789ff5ce0df53d6d63b009d834f1adddd1e506
Erik Dahl <erik@iunderlandet.se>
2025-09-12 12:36:15 +0200

    feat: add status GET endpoint to rest_server

---
+++ controlunit/components/rest_server/handlers/status_handler.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "esp_http_server.h"
+
+esp_err_t status_get_handler(httpd_req_t *req);
--- controlunit/components/rest_server/rest_server.cpp
+++ controlunit/components/rest_server/rest_server.cpp
@@ -1,5 +1,6 @@
 #include "rest_server.h"
 #include "hello_handler.h"
+#include "status_handler.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
 
@@ -12,6 +13,7 @@ void start_rest_server() {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
     if (httpd_start(&server, &config) == ESP_OK) {
+
         httpd_uri_t hello_uri = {
             .uri      = "/hello",
             .method   = HTTP_GET,
@@ -19,6 +21,15 @@ void start_rest_server() {
             .user_ctx = NULL
         };
         httpd_register_uri_handler(server, &hello_uri);
+
+        httpd_uri_t status_uri = {
+            .uri      = "/status",
+            .method   = HTTP_GET,
+            .handler  = status_get_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &status_uri);
+
         ESP_LOGI(TAG, "Server started");
     } else {
         ESP_LOGE(TAG, "Failed to start server");
