368e5f83c290f016ec6809b69c45340d48766b43
Erik Dahl <erik@iunderlandet.se>
2025-10-07 15:08:41 +0200

    docs: add doxygen comments to connection_data_types

--- controlunit/components/connection_data/connection_data_types.cpp
+++ controlunit/components/connection_data/connection_data_types.cpp
@@ -1,5 +1,21 @@
+/**
+ * @file connection_data_types.cpp
+ * @author Erik Dahl (erik@iunderlandet.se)
+ * @brief Implementation of helper functions for sensor data communication
+ * @date 2025-10-07
+ * 
+ * @copyright Copyright (c) 2025 Erik Dahl
+ * @license MIT
+ * 
+ */
 #include "connection_data_types.h"
 
+/**
+ * @brief Return a string corresponding to a requestType
+ * 
+ * @param request 
+ * @return std::string 
+ */
 std::string requestTypeToString(requestType request){
     switch (request) {
         case requestType::CONNECT:    return "connect";
@@ -8,6 +24,12 @@ std::string requestTypeToString(requestType request){
     }
 }
 
+/**
+ * @brief Return a string corresponding to a connectionStatus
+ * 
+ * @param status 
+ * @return std::string 
+ */
 std::string connectionStatusToString(connectionStatus status) {
     switch (status) {
         case connectionStatus::CONNECTED:    return "connected";
--- controlunit/components/connection_data/connection_data_types.h
+++ controlunit/components/connection_data/connection_data_types.h
@@ -1,22 +1,56 @@
+/**
+ * @file connection_data_types.h
+ * @author Erik Dahl (erik@iunderlandet.se)
+ * @brief Structs and enums for handling communication between Control Unit,
+ * Backend and Sensor Units
+ * @date 2025-10-07
+ * 
+ * @copyright Copyright (c) 2025 Erik Dahl
+ * @license MIT
+ * 
+ */
 #pragma once
 #include "sensor_data_types.h"
 
+/**
+ * @brief Valid request for connecting a Driver as well as Sensor Unit
+ * 
+ */
 enum class requestType { CONNECT, DISCONNECT };
 
+/**
+ * @brief Helper function to convert a requestType to a string
+ * 
+ * @param request 
+ * @return std::string 
+ */
 std::string requestTypeToString(requestType request);
 
+/**
+ * @brief Struct used for composing a Sensor Connect Request
+ * 
+ */
 struct SensorConnectRequest {
     std::shared_ptr<Uuid> sensorUuid;
     requestType           request;
     std::string           token;
 };
 
+/**
+ * @brief Struct used for composing a Driver Connect Request
+ * 
+ */
 struct DriverConnectRequest {
     uint32_t    driverId {};
     requestType request;
     std::string token;
 };
 
+/**
+ * @brief Valid status for communicating connection status
+ * for both Sensor Unit and Driver
+ * 
+ */
 enum class connectionStatus {
     CONNECTED,
     DISCONNECTED,
@@ -24,13 +58,27 @@ enum class connectionStatus {
     UNAVAILABLE,
 };
 
+/**
+ * @brief Helper function to convert a connectionStatus to a string
+ * 
+ * @param status 
+ * @return std::string 
+ */
 std::string connectionStatusToString(connectionStatus status);
 
+/**
+ * @brief Struct used for composing a Sensor Unit Connect Response
+ * 
+ */
 struct SensorConnectResponse {
     std::shared_ptr<Uuid> sensorUuid;
     connectionStatus      status;
 };
 
+/**
+ * @brief Struct used for composing a Driver Connect Response
+ * 
+ */
 struct DriverConnectResponse {
     uint32_t         driverId;
     connectionStatus status;
