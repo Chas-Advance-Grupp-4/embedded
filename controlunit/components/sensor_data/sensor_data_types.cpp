e0dc2627aac0c71ea4da27b36ce89b9a481c1329
Erik Dahl <erik@iunderlandet.se>
2025-10-07 16:04:27 +0200

    docs: add doxygen comments to sensor_data_types

--- controlunit/components/sensor_data/sensor_data_types.cpp
+++ controlunit/components/sensor_data/sensor_data_types.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file sensor_data_types.cpp
+ * @author Erik Dahl (erik@iunderlandet.se)
+ * @brief Implements utility functions for UUID comparison and validation.
+ *
+ * Provides the implementation of Uuid methods such as toString(), isValid(),
+ * and comparison operators.
+ * @date 2025-10-07
+ *
+ * @copyright Copyright (c) 2025 Erik Dahl
+ * @license MIT
+ *
+ */
 #include "sensor_data_types.h"
 
 const std::string& Uuid::toString() const {
