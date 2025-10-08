bd0ae0406f196283ed130cf407d613c7434a7606
Erik Dahl <erik@iunderlandet.se>
2025-10-08 19:33:44 +0200

    feat: JsonParser that composes JSON from sensor readings

--- sensorunit/src/main.cpp
+++ sensorunit/src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
+#include "JsonParser.h"
+#include "constants.h"
+#include "config.h"
+#include "sensor_data_types.h"
 #include <etl/string.h>
 #include <etl/vector.h>
 
 int             counter{0};
 etl::string<32> hello{};
+etl::vector<ca_sensorunit_reading, MAX_BATCH_SIZE> testReadings;
 
 void setup() {
+    testReadings.push_back({ 1726995605, 25, 50 });
+    testReadings.push_back({ 1726995610, 30, 55 });
     hello.append("Hello ETL Arduino No ");
 
     Serial.begin(115200);
@@ -14,6 +21,9 @@ void setup() {
     Serial.print(hello.c_str());
     Serial.println(counter);
     counter++;
+
+    Serial.print("JsonParser good luck now: ");
+    Serial.println(JsonParser::composeSensorSnapshotGroup(testReadings, SENSOR_UNIT_ID).c_str());
 }
 
 void loop() {
