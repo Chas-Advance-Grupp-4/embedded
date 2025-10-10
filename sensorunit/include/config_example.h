82916c3be65a207c5b04250d1a49c1d5356ca499
Erik Dahl <erik@iunderlandet.se>
2025-10-10 13:06:42 +0200

    feat: add ESP style logging macros

--- sensorunit/include/config_example.h
+++ sensorunit/include/config_example.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// Copy to config.h and enter your own values
+// Copy this file to config.h and enter your own values
+
+#define LOG_LEVEL LOG_INFO
+
 constexpr const char* SENSOR_UNIT_ID = "REPLACE_WITH_UUID";
 constexpr const char* WIFI_SSID = "REPLACE_WITH_SSID";
 constexpr const char* WIFI_PASSWORD = "REPLACE_WITH_PASSWORD";
